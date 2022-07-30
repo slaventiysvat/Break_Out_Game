8d97055bb060ce0edf4a8bb7c197d5d9262abdc4
Sviatoslav Starokozhev <slaventiysvat@gmail.com>
2022-07-30 14:38:29 +0300

    added initial commit and files

---
+++ Break_Out_Game/Break_Out_Game/Break_Out_Game.cpp
@@ -0,0 +1,12 @@
+﻿// Break_Out_Game.cpp: определяет точку входа для приложения.
+//
+
+#include "Break_Out_Game.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
