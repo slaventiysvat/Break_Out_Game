3fdf2c8cca23cecf94c76182ceaa02d2ac477296
Sviatoslav Starokozhev <slaventiysvat@gmail.com>
2022-07-31 18:43:22 +0300

    realized base game without game menu

--- Break_Out_Game/Break_Out_Game/Break_Out_Game.cpp
+++ Break_Out_Game/Break_Out_Game/Break_Out_Game.cpp
@@ -10,13 +10,82 @@
 static const int Levels = 3;
 static const int Lives = 5;
 
-
+std::string level_test_bounce;
+std::string level_1;
+std::string level_2;
+std::string level_3;
 
 int main()
 {
+
+	level_test_bounce =  "################";
+	level_test_bounce += "#..............#";
+	level_test_bounce += "#...332111323..#";
+	level_test_bounce += "#..............#";
+	level_test_bounce += "#..............#";
+	level_test_bounce += "#..............#";
+	level_test_bounce += "#..............#";
+	level_test_bounce += "#..............#";
+	level_test_bounce += "#..............#";
+	level_test_bounce += "#..............#";
+	level_test_bounce += "#..............#";
+	level_test_bounce += "#..............#";
+	level_test_bounce += "#..............#";
+	level_test_bounce += "#..............#";
+	level_test_bounce += "################";
+
+	level_1 = "################";
+	level_1 += "#...33111111...#";
+	level_1 += "#...33122133...#";
+	level_1 += "#..............#";
+	level_1 += "#..............#";
+	level_1 += "#..............#";
+	level_1 += "#..............#";
+	level_1 += "#..............#";
+	level_1 += "#..............#";
+	level_1 += "#..............#";
+	level_1 += "#..............#";
+	level_1 += "#..............#";
+	level_1 += "#..............#";
+	level_1 += "#..............#";
+	level_1 += "#..............#";
+
+	level_2 = "################";
+	level_2 += "#...11111111...#";
+	level_2 += "#..............#";
+	level_2 += "#...12121212...#";
+	level_2 += "#..............#";
+	level_2 += "#..............#";
+	level_2 += "#...13212313...#";
+	level_2 += "#..............#";
+	level_2 += "#..............#";
+	level_2 += "#..............#";
+	level_2 += "#..............#";
+	level_2 += "#..............#";
+	level_2 += "#..............#";
+	level_2 += "#..............#";
+	level_2 += "#..............#";
+
+	level_3 = "################";
+	level_3 += "#...33333333...#";
+	level_3 += "#...11111111...#";
+	level_3 += "#...32211223...#";
+	level_3 += "#...11111111...#";
+	level_3 += "#...11111111...#";
+	level_3 += "#...11111111...#";
+	level_3 += "#..............#";
+	level_3 += "#..............#";
+	level_3 += "#..............#";
+	level_3 += "#..............#";
+	level_3 += "#..............#";
+	level_3 += "#..............#";
+	level_3 += "#..............#";
+	level_3 += "#..............#";
+
 	std::cout << "Hello CMake." << std::endl;
 
-	Brekout Game_Breakout;
+	//Brekout Game_Breakout(level_test_bounce);
+	Brekout Game_Breakout(level_1);
 	Game_Breakout.ConstructConsole(256, 240, 4, 4);
 	Game_Breakout.Start();
 
--- Break_Out_Game/Break_Out_Game/Breakout.cpp
+++ Break_Out_Game/Break_Out_Game/Breakout.cpp
@@ -1,11 +1,6 @@
 #include "Breakout.h"
 #include "engine_constants.h"
 
-std::string level_test_bounce;
-std::string level_1;
-std::string level_2;
-std::string level_3;
-
 
 Ball::Ball() {
 	float fAng = (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * PI * 2.0f;
@@ -14,10 +9,10 @@ Ball::Ball() {
 	fBallDY = sinf(fAng);
 }
 
-ball_coordinates Ball::BallMove(float fElapsedTime, int nBlock,int nWidth, std::string level) {
+ void Ball::BallMove(float fElapsedTime, int nBlock,int nWidth, int nHeight,float fBat, std::string &level) {
 
-	ball_coordinates out_coord;
 
+	int nBatWidth = 15;
 	fOldX = fBallX;
 	fOldY = fBallY;
 	fBallX = fBallX + fBallDX * fElapsedTime * fSpeed;
@@ -32,100 +27,92 @@ ball_coordinates Ball::BallMove(float fElapsedTime, int nBlock,int nWidth, std::
 	char newcell = level.at(nCellNewY * nWidth + nCellNewX);
 	char oldcell = level.at(nCellOldY * nWidth + nCellOldX);
 	//Testing Collision Ball and Level Border
-	if (newcell == '#') {
+	//if (newcell == '#') {
 
-		if (nCellNewX != nCellOldX) {
+	//	if (nCellNewX != nCellOldX) {
 
-			fBallDX = fBallDX * (-1.0f);
+	//		fBallDX = fBallDX * (-1.0f);
 
+	//	}
+
+	//	if (nCellNewY != nCellOldY) {
+
+	//		fBallDY = fBallDY * (-1.0f);
+
+	//	}
+
+	//}
+
+	if (newcell != '.') {
+
+		if (newcell == '3') {
+			level.at(nCellNewY * nWidth + nCellNewX) = '2';
 		}
 
-		if (nCellNewY != nCellOldY) {
-
-			fBallDY = fBallDY * (-1.0f);
-
+		if (newcell == '2') {
+			level.at(nCellNewY * nWidth + nCellNewX) = '1';
 		}
+
+		if (newcell == '1') {
+			level.at(nCellNewY * nWidth + nCellNewX) = '.';
+		}
+
+		if (nCellNewX != nCellOldX) { fBallDX = fBallDX * (-1.0f); }
+		if (nCellNewY != nCellOldY) { fBallDY = fBallDY * (-1.0f); }
 
 	}
 
-	out_coord.ball_x = fBallX;
-	out_coord.ball_y = fBallY;
 
-	return out_coord;
+	//Ball with Platform 
+	if (fBallY > (nHeight * nBlock - 2))
+	{
+		if ((fBallX != (fBat - nBatWidth)) && (fBallX > (fBat - nBatWidth))) {
+
+			fBallDY = fBallDY * (-1.0f);
+			std::cout << "nice job" << std::endl;
+		}
+		else {
+
+			//Dead, 
+			//Life minus
+			//Ball Respawn on Middle of the Screen
+			fBallX = (nWidth / 2.0f) * nBlock;
+			fBallY = (nHeight / 2.0f) * nBlock;
+			//Change random angle
+			float fAng = (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * PI * 2.0f;//New angle after meeting each other
+			//fAng = 0.6f;
+			fBallDX = cosf(fAng);
+			fBallDY = sinf(fAng);
+			std::cout << "you deatch" << std::endl;
+		}
+	}
+
 }
 
+//void  Ball::update_ball_state(ball_coordinates new_state) {
+//
+//	Ball::fBallDX = new_state.ball_dx;
+//	Ball::fBallDY = new_state.ball_dy;
+//	Ball::fBallX = new_state.ball_x;
+//	Ball::fBallY = new_state.ball_y;
+//	//Ball::fOldX = new_state.ball_old_dx;
+//	//Ball::fOldY = new_state.ball_old_dy;
+//}
 
 bool Brekout::OnUserCreate() {
 	//	
 
-	level_test_bounce = "################";
-	level_test_bounce += "#..............#";
-	level_test_bounce += "#..............#";
-	level_test_bounce += "#..............#";
-	level_test_bounce += "#..............#";
-	level_test_bounce += "#..............#";
-	level_test_bounce += "#..............#";
-	level_test_bounce += "#..............#";
-	level_test_bounce += "#..............#";
-	level_test_bounce += "#..............#";
-	level_test_bounce += "#..............#";
-	level_test_bounce += "#..............#";
-	level_test_bounce += "#..............#";
-	level_test_bounce += "#..............#";
-	level_test_bounce += "################";
-
-	level_1 =  "################";
-	level_1 += "#...11111111...#";
-	level_1 += "#..............#";
-	level_1 += "#...22222222...#";
-	level_1 += "#..............#";
-	level_1 += "#..............#";
-	level_1 += "#...33333333...#";
-	level_1 += "#..............#";
-	level_1 += "#..............#";
-	level_1 += "#..............#";
-	level_1 += "#..............#";
-	level_1 += "#..............#";
-	level_1 += "#..............#";
-	level_1 += "#..............#";
-	level_1 += "#..............#";
-
-	level_2 = "################";
-	level_2 += "#...11111111...#";
-	level_2 += "#..............#";
-	level_2 += "#...12121212...#";
-	level_2 += "#..............#";
-	level_2 += "#..............#";
-	level_2 += "#...13212313...#";
-	level_2 += "#..............#";
-	level_2 += "#..............#";
-	level_2 += "#..............#";
-	level_2 += "#..............#";
-	level_2 += "#..............#";
-	level_2 += "#..............#";
-	level_2 += "#..............#";
-	level_2 += "#..............#";
-
-	level_3 = "################";
-	level_3 += "#...33333333...#";
-	level_3 += "#...11111111...#";
-	level_3 += "#...32211223...#";
-	level_3 += "#...11111111...#";
-	level_3 += "#...11111111...#";
-	level_3 += "#...11111111...#";
-	level_3 += "#..............#";
-	level_3 += "#..............#";
-	level_3 += "#..............#";
-	level_3 += "#..............#";
-	level_3 += "#..............#";
-	level_3 += "#..............#";
-	level_3 += "#..............#";
-	level_3 += "#..............#";
-
 	//Ball Ball_Obj;
+
+	float fAng = (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * PI * 2.0f;
+	fAng = 0.6f;
+	Ball_Obj.set_fball_dx(cosf(fAng));
+	Ball_Obj.set_fball_dy(sinf(fAng));
 
 	return true;
 }
+
+
 
 
 
@@ -140,17 +127,29 @@ bool Brekout::OnUserUpdate(float fElapsedTime) {
 	if (GetKey(VK_LEFT).bHeld) { fBat = fBat - fbatSpeed * fElapsedTime; }
 	if (GetKey(VK_RIGHT).bHeld) { fBat = fBat + fbatSpeed * fElapsedTime; }
 
+
+	if ((fBat - nBatWidth) < nBlock) {
+
+		fBat = nBlock + nBatWidth;
+
+	}
+
+	if ((fBat + nBatWidth) > (nWidth * nBlock)) {
+
+		fBat = nWidth * nBlock - nBatWidth;
+
+	}
 	//Ball Moving
-	ball_coordinates coordinantes_ball = Ball_Obj.BallMove(fElapsedTime, nBlock, Brekout::nWidth,level_test_bounce);
+	Ball_Obj.BallMove(fElapsedTime, nBlock,nWidth,nHeight, fBat, level);
 
 	//Drawing Elements
 	// -------------------------------------------------------------------
 	//Draw Level;
-	DrawLevel(level_test_bounce, Brekout::nHeight, Brekout::nWidth, Brekout::nBlock);
+	DrawLevel(Brekout::level, Brekout::nHeight, Brekout::nWidth, Brekout::nBlock);
 	//DrawLevel(level_3, Brekout::nHeight, Brekout::nWidth, Brekout::nBlock);
 
 	//BallDrawing
-	FillCircle(coordinantes_ball.ball_x, coordinantes_ball.ball_y, 2.0f, PIXEL_SOLID, FG_BLUE);
+	FillCircle(Ball_Obj.get_fball_x(), Ball_Obj.get_fball_y(), 2.0f, PIXEL_SOLID, FG_BLUE);
 
 	//Platform Drawing
 	DrawLine(fBat - nBatWidth, nHeight * nBlock - 2, fBat + nBatWidth, nHeight * nBlock - 2, PIXEL_SOLID, FG_WHITE);
